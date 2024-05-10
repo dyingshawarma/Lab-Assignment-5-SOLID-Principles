cca1b6782082fde9a47b63c3dc4297c9dac13328
Charlene Manning <113045523+dyingshawarma@users.noreply.github.com>
2024-05-10 23:52:36 +0800

    Create InvoiceAction.java

---
+++ InvoiceAction.java
@@ -0,0 +1,8 @@
+public class InvoiceAction implements InvoiceOperations {
+
+  @Override
+  public void generateInvoice(String fileName) {
+    // Simulate generating invoice file
+    System.out.println("Invoice generated: " + fileName);
+  }
+}
